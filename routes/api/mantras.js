eba64bc49ef36d260fcdfb10387df10c2afea496
erikacolette <erikatidwell.c@gmail.com>
2021-01-15 15:53:19 -0500

    backend tockens

--- routes/api/mantras.js
+++ routes/api/mantras.js
@@ -2,12 +2,20 @@ const express = require("express");
 const router = express.Router();
 const mantrasCtrl = require("../../controllers/api/mantras");
 
-/* GET /api/mantras */
-
+// Public Routes
 router.get("/", mantrasCtrl.index);
+
+// Protected Routes
+router.use(require('../../config/auth'));
 router.get("/:id", mantrasCtrl.show);
 router.post("/", mantrasCtrl.create);
 router.delete("/:id", mantrasCtrl.delete);
 router.put("/:id", mantrasCtrl.update);
+
+
+function checkAuth(req, res, next) {
+	if (req.user) return next();
+	return res.status(401).json({msg: 'Not Authorized'});
+}
 
 module.exports = router;
