6219a6c0965be3d02b4db9cf0ebc6c773e0c2c9d
sebastians9517 <sebastians9517@gmail.com>
2021-01-16 11:30:25 -0600

    add affirmation card jsx complete

--- src/components/AddAffirmationCard/AddAffirmationCard.jsx
+++ src/components/AddAffirmationCard/AddAffirmationCard.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useRef, useState } from 'react';
 import { useForm } from "../../hooks/useForm";
 
 export default function AddAffirmationCard(props) {
@@ -7,7 +7,27 @@ export default function AddAffirmationCard(props) {
         text: '',
     });
 
+    const formRef = useRef();
 
+    useEffect(() => {
+        formRef.current.checkValidity() ? setValidForm(false) : setValidForm(true);
+    }, [state]);
 
-
-}
+    return (
+        <>
+            <h1>Add an Affirmation</h1>
+            <form autoComplete="off" ref={formRef} onSubmit={(e) => {
+                e.preventDefault();
+                props.handleAddAffirmation(state);
+            }}
+            >
+                <div>
+                    <label>Affirmation(required)</label>
+                    <input name="text" value={state.text} onChange={handleChange} required
+                    />
+                </div>
+                <button type="submit" disabled={invalidForm}>ADD</button>
+            </form>
+        </>
+    );
+};
