904ea7e064141f301ef7966b08c3e181927429e4
sebastians9517 <sebastians9517@gmail.com>
2021-01-16 13:03:35 -0600

    api routes complete

--- routes/api/affirmations.js
+++ routes/api/affirmations.js
@@ -8,3 +8,13 @@ router.get("/", affirmationsCtrl.index);
 // Protected routes
 router.use(require('../../config/auth'));
 router.get("/:id", affirmationsCtrl.show);
+router.post("/", affirmationsCtrl.create);
+router.delete("/:id", affirmationsCtrl.delete);
+router.put("/:id", affirmationsCtrl.update);
+
+function checkAuth(req, res, next) {
+    if (req.user) return next();
+    return res.status(401).json({msg: "Not Authorized"});
+};
+
+module.exports = router;
