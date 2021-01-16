cc396df8367e3267cf98a6e06abe82ccdd7fe6c9
sebastians9517 <sebastians9517@gmail.com>
2021-01-16 11:54:53 -0600

    create affirmation model file

---
+++ models/affirmation.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const affirmationSchema = new Schema({
+    text:
+})
--- models/mantra.js
+++ models/mantra.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const mantraSchema = new Schema({
-    // id
     text: {type: String, required: true}
 
 },{
