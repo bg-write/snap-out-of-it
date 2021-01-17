1a66f8ff48066d803d942918119b3d927f9598e7
sebastians9517 <sebastians9517@gmail.com>
2021-01-17 12:14:21 -0600

    quotes test

--- controllers/api/quotessassy.js
+++ controllers/api/quotessassy.js
@@ -4,3 +4,15 @@ module.exports = {
     index,
     show
 }
+
+// setting all quotes to variable and turning variable into json obj
+async function index(req,res){
+    const quotesSassy = await QuotesSassy.find({})
+    res.status(200).json(quotesSassy)
+}
+
+// finding individual quote by id and set it to a variable that turns into a json obj
+async function show(req, res){
+    const quotesSassy = await QuotesSassy.findById(req.params.id)
+    res.status(200).json(quotesSassy)
+}
