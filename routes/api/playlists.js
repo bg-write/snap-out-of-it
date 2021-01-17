06b5c713d83b8ad95964ecba6b593df37a162655
sebastians9517 <sebastians9517@gmail.com>
2021-01-16 18:58:29 -0600

    router index variable in playlist-api routes

--- routes/api/playlists.js
+++ routes/api/playlists.js
@@ -1,3 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const playlistsCtrl = require("../../controllers/api/playlists");
+
+// Public routes
+router.get("/", playlistsCtrl.index);
