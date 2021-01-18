3716201d9edf29a79cb8adc80015b16fd12576c7
bg-write <bradywgerber@gmail.com>
2021-01-18 13:39:56 -0500

    Create working API random quote generator

--- src/components/Quotes/Quotes.jsx
+++ src/components/Quotes/Quotes.jsx
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Quotes = () => {
+	const [quote, setQuote] = useState('');
+	const [author, setAuthor] = useState('');
+
+	useEffect(() => {
+		getQuote();
+	}, []);
+
+	const getQuote = () => {
+		let url =
+			'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => {
+				let dataQuotes = data.quotes;
+				let randomNum = Math.floor(Math.random() * dataQuotes.length);
+				let randomQuote = dataQuotes[randomNum];
+
+				setQuote(randomQuote.quote);
+				setAuthor(randomQuote.author);
+			});
+	};
+
+	const handleClick = () => {
+		getQuote();
+	};
+
 	return (
 		<>
 			<div id="quote-box">
 				<div id="text">
-					<p>THIS IS A PLACEHOLDER FOR QUOTE TEXT WOWOWOWOOWOWOOWOWOW</p>
+					<p>{quote}</p>
 				</div>
 				<div id="author">
-					<p>John Doe</p>
+					<p>{author}</p>
 				</div>
-                    <button id="new-quote">New Quote</button>
+				<button onClick={handleClick} id="new-quote">
+					New Quote
+				</button>
 			</div>
 		</>
 	);
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,41 +8,9 @@ import Quotes from '../../components/Quotes/Quotes'
 // Here we import all the things we are exporting from all our pages
 
 function HomePage({ user }) {
-  // const [quotesSassy, setQuoteSassy] = useState([]);
-  // const history = useHistory();
-
-  // useEffect(() => {
-  //   history.push("/");
-  // }, [quotesSassy, history]);
-
-  /*--- Lifecycle Methods ---*/
-  // useEffect(() => {
-  //   (async function () {
-  //     const quotesSassy = await quotesSassyAPI.getAll();
-  //     setQuoteSassy(quotesSassy);
-  //   })();
-  // }, [quotesSassy]);
-
-  // function randomQuoteSassy() {
-  //   const randomNumber = Math.floor(Math.random() * quotesSassy.length);
-  //   return quotesSassy[randomNumber];
-  // }
-
-  // function shuffleQuotesSassy(array) {
-  //   return array.sort(() => Math.random() - 0.5);
-  // }
-
-  // function handleClick() {
-  //   const generateRandomQuoteSassy = this.randomQuoteSassy();
-  //   this.setState({
-  //     quotesSassy: generateRandomQuoteSassy.quotesSassy,
-  //   });
-  //   this.shuffleQuotesSassy(quotesSassy);
-  // }
 
   return (
     <>
-      <header></header>
       <main>
         <p className="nav-link">Welcome, {user.name}</p>
         <h1>Home actual page</h1>
