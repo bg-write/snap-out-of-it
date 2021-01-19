e290fc78fec78bba5808bc89387d0557de7f61bc
bg-write <bradywgerber@gmail.com>
2021-01-18 21:49:44 -0500

    Update sassy quotes

---
+++ src/components/QuotesSassy/QuotesSassyCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class QuoteAndAuthor extends React.Component {
+	render() {
+		return (
+			<div id="quote-box">
+				<div key={Math.random()}>
+					<h1 id="text">"{this.props.quote}"</h1>
+					<h5 id="author">
+						{this.props.author ? this.props.author : 'Unknown'}
+					</h5>
+				</div>
+				<button onClick={this.props.handleClick} id="new-quote">
+					Slap!
+				</button>
+			</div>
+		);
+	}
+}
+
+export default QuoteAndAuthor;
