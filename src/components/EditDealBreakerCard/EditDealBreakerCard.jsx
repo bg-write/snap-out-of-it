2f6b5bee51521a0b1bdde26356099dfb13960c02
sebastians9517 <sebastians9517@gmail.com>
2021-01-21 11:36:21 -0600

    edit input does not repeat the text anymore

--- src/components/EditDealBreakerCard/EditDealBreakerCard.jsx
+++ src/components/EditDealBreakerCard/EditDealBreakerCard.jsx
@@ -29,7 +29,7 @@ export default function EditDealBreakerCard({
           <input
             className="form-control input-edit-deal form-control-md"
             name="text"
-            value={state.text}
+            // value={state.text}
             onChange={handleChange}
             required
           />
